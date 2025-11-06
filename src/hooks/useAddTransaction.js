2bd2716507e80d1ff6add88a9fb8582c348aebef
Yusuf Neeson <yusufneeson@gmail.com>
2025-11-06 18:24:40 +0700

    fix useAddTransaction

--- src/hooks/useAddTransaction.js
+++ src/hooks/useAddTransaction.js
@@ -2,30 +2,30 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 export function useAddTransaction() {
-	const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-	return useMutation({
-		mutationFn: async (transaction) => {
-			const req = await fetch("/api/transaction", {
-				method: "POST",
-				body: JSON.stringify(transaction),
-			});
+  return useMutation({
+    mutationFn: async (transaction) => {
+      const req = await fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(transaction),
+      });
 
-			if (!req.ok) {
-				throw new Error(`Response status: ${response.status}`);
-			}
+      if (!req.ok) {
+        throw new Error(`Response status: ${req.status}`);
+      }
 
-			const resp = await req.json();
+      const resp = await req.json();
 
-			return resp;
-		},
-		onSuccess: () => {
-			toast.success("Transaksi berhasil ditambahkan");
-			queryClient.invalidateQueries({ queryKey: ["userData"] });
-		},
-		onError: (error) => {
-			console.error(error);
-			toast.error("Gagal menambah transaksi");
-		},
-	});
+      return resp;
+    },
+    onSuccess: () => {
+      toast.success("Transaksi berhasil ditambahkan");
+      queryClient.invalidateQueries({ queryKey: ["userData"] });
+    },
+    onError: (error) => {
+      console.error(error);
+      toast.error("Gagal menambah transaksi");
+    },
+  });
 }
