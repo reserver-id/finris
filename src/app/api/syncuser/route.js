fb2354ad011430008fcc0224bfaacb2f4930a607
yusufneeson <yusufneeson@gmail.com>
2025-11-06 15:01:58 +0700

    fix api syncuser error
    Signed-off-by: yusufneeson <yusufneeson@gmail.com>

--- src/app/api/syncuser/route.js
+++ src/app/api/syncuser/route.js
@@ -5,19 +5,19 @@ export async function POST() {
 	try {
 		const clerkUser = await currentUser();
 
-		if (!user || !user.emailAddresses[0]?.emailAddress) {
+		if (!clerkUser || !clerkUser.emailAddresses[0]?.emailAddress) {
 			return Response.json({ error: "No user info" }, { status: 401 });
 		}
 
-		const email = user.emailAddresses[0].emailAddress;
-		const name = user.firstName
-			? `${user.firstName} ${user.lastName}`
+		const email = clerkUser.emailAddresses[0].emailAddress;
+		const name = clerkUser.firstName
+			? `${clerkUser.firstName} ${clerkUser.lastName}`
 			: "RESERVER";
 		const clerk_id = clerkUser.id;
 
 		const user = await getOrCreateUser({ clerk_id, email, name });
 
-		return Response.json(inserted);
+		return Response.json(user);
 	} catch (error) {
 		console.error(error);
 		return Response.json({ error: error.message }, { status: 500 });
